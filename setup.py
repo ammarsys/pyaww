e6b361ebf09a5c4be30b89c29f7bacb171e1845d
Ammar-sys <amarftw1@gmail.com>
2021-07-12 01:37:46 +0200

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyaww",
-    version="0.0.1",
+    version="0.0.2",
     author="ammarsys",
     author_email="amarftw1@gmail.com",
     description="A simple API wrapper around the pythonanywhere's API.",
