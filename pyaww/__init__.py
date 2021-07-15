ea08fe5594d1ab6a464ab73225ff8902883418fe
Ammar-sys <amarftw1@gmail.com>
2021-07-15 14:40:22 +0200

    update version 0.0.2 -> 0.0.3

--- pyaww/__init__.py
+++ pyaww/__init__.py
@@ -9,4 +9,4 @@ from pyaww import (
     webapp
 )
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyaww",
-    version="0.0.2",
+    version="0.0.3",
     author="ammarsys",
     author_email="amarftw1@gmail.com",
     description="A simple API wrapper around the pythonanywhere's API.",
