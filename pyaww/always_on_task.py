cf97483965579c5e0ff30e699e183638766dcfb0
Ammar <amarftw1@gmail.com>
2021-10-23 14:08:35 +0200

    [update] typing and google docstrings

--- pyaww/always_on_task.py
+++ pyaww/always_on_task.py
@@ -1,9 +1,11 @@
 """Class for the always on tasks API endpoints"""
 
 # Standard library imports
+
 from typing import TYPE_CHECKING
 
 # Local application/library specific imports
+
 from .sched_task import SchedTask
 
 if TYPE_CHECKING:
@@ -11,13 +13,17 @@ if TYPE_CHECKING:
 
 
 class AlwaysOnTask(SchedTask):
-    """Inherits sheduled task class because they're pretty similar. Only 1 method difference."""
-    def __init__(self, resp: dict, user: 'User') -> None:
+    """Inherits sheduled task."""
+
+    def __init__(self, resp: dict, user: "User") -> None:
         super().__init__(resp, user)
 
     def restart(self) -> None:
-        """Restart a always_on task."""
-        self._user.request('POST', self.url + 'restart/')
+        """Restart an always_on task."""
+        self._user.request("POST", self.url + "restart/")
 
     def __str__(self):
         return self.url
+
+    def __eq__(self, other):
+        return self.url == other.url
