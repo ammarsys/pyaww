485c32cc35ff51aa472824c9ec3771ef84e18a36
Ammar <amarftw1@gmail.com>
2021-10-23 14:07:31 +0200

    [update] bump version

--- pyaww/__init__.py
+++ pyaww/__init__.py
@@ -1,11 +1,3 @@
-"""
-A lightweight API wrapper around the PythonAnywhere's API.
-
-The documentations are located at https://ammarsys.github.io/pyaww-docs/
-
-License: MIT
-"""
-
 from pyaww import (
     always_on_task,
     console,
@@ -14,7 +6,7 @@ from pyaww import (
     static_header,
     static_file,
     user,
-    webapp
+    webapp,
 )
 
-__version__ = '0.0.4'
+__version__ = "0.0.4"
