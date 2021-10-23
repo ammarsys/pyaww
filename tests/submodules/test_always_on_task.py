4803a843a8d4e4e8a457996ea186ee3eafbc5921
Ammar <amarftw1@gmail.com>
2021-10-23 17:36:10 +0200

    [patch] seperate methods

--- tests/submodules/test_always_on_task.py
+++ tests/submodules/test_always_on_task.py
@@ -1,7 +1,12 @@
 # Local application/library specific imports
 
-from pyaww.user import AlwaysOnTask
+from pyaww.user import AlwaysOnTask, User
 
 
 def test_restart(always_on_task: AlwaysOnTask) -> None:
     assert always_on_task.restart() is None
+
+
+def test_get_always_on_task_by_id(client: User, always_on_task: AlwaysOnTask) -> None:
+    assert client.get_always_on_task_by_id(always_on_task.id) == always_on_task
+
