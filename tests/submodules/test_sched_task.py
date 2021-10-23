8dfc3af4916ae15fa3f00479dd502c8dc8cfa5a2
Ammar <amarftw1@gmail.com>
2021-10-23 17:36:28 +0200

    [patch] seperate methods

--- tests/submodules/test_sched_task.py
+++ tests/submodules/test_sched_task.py
@@ -1,12 +1,16 @@
 # Local application/library specific imports
 
-from pyaww.user import SchedTask
+from pyaww.user import SchedTask, User
 
 
 def test_update(scheduled_task: SchedTask) -> None:
     scheduled_task.update(description="A")
     scheduled_task.update(description="B")
     assert scheduled_task.description == "B"
+
+
+def test_get_sched_task_by_id(client: User, scheduled_task: SchedTask) -> None:
+    assert client.get_sched_task_by_id(scheduled_task.id) == scheduled_task
 
 
 def test_delete(scheduled_task: SchedTask) -> None:
