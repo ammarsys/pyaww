78098b1d3f6b2a23db5b4a5da500b50f86ac3044
Ammar <amarftw1@gmail.com>
2021-10-23 14:09:02 +0200

    [update] typing and google docstrings

--- pyaww/console.py
+++ pyaww/console.py
@@ -1,17 +1,20 @@
 """Class for the console API endpoints"""
 
 # Standard library imports
+
 from typing import TYPE_CHECKING
 
 # Local application/library specific imports
+
 if TYPE_CHECKING:
     from .user import User
 
 
 class Console:
     """All methods of a console."""
+
     id: int
-    user: 'User'
+    user: "User"
     executable: str
     arguments: str
     working_directory: str
@@ -19,40 +22,44 @@ class Console:
     console_url: str
     console_frame_url: str
 
-    def __init__(self, resp: dict, user: 'User') -> None:
-        """
-        Initialize class variables.
-
-        :param dict resp: json dictionary returned by the API
-        :param user: User class (see pyaww.user)
-        """
+    def __init__(self, resp: dict, user: "User") -> None:
         vars(self).update(resp)
         self._user = user
 
-    def send_input(self, inp: str, end: str = '\n') -> str:
+    def send_input(self, inp: str, end: str = "\n") -> str:
         """
-        Function to send inputs to a console.
+        Function to send inputs to the console. Console must be started manually before hand.
 
-        Sample usage -> Console.send_input("print('hello!')", end='')
+        Args:
+            inp (str): string to be inputted in the console
+            end (str): pass '' to not "click enter" in console
 
-        :param str inp: string to be inputted in the console
-        :param bool end: pass '' to not "click enter" in console
-        :return: latest output (output of that code)
+        Examples:
+            >>> console = User(...).get_console_by_id(...)
+            >>> console.send_input("print('hello!')", end='')
+
+        Returns:
+            str: latest writting in the console
         """
         self._user.request(
-            'POST',
-            '/api/v0' + self.console_url + f'send_input/',
-            data={'input': inp + end}
+            "POST",
+            "/api/v0" + self.console_url + f"send_input/",
+            data={"input": inp + end},
         )
-        return self.outputs().split('\r')[-2].strip()
+
+        return self.outputs().split("\r")[-2].strip()
 
     def delete(self) -> None:
-        """Function to delete a console. Sample usage -> Console.delete()"""
-        self._user.request('DELETE', '/api/v0' + self.console_url)
+        """Delete the console."""
+        self._user.request("DELETE", "/api/v0" + self.console_url)
 
     def outputs(self) -> str:
-        """Functions to return all outputs in a console. Sample usage -> Console.outputs()"""
-        return self._user.request('GET', '/api/v0' + self.console_url + 'get_latest_output/').json()['output']
+        """Return all outputs in a console."""
+        resp = self._user.request(
+            "GET", "/api/v0" + self.console_url + "get_latest_output/"
+        ).json()
+
+        return resp["output"]
 
     def __str__(self):
         return self.console_url
