5c86e0bf43d284566bb878829cd4b390bb88ae67
Ammar <amarftw1@gmail.com>
2021-10-23 14:05:54 +0200

    [update] typing and google docstrings

--- utils/utils.py
+++ utils/utils.py
@@ -1,58 +1,94 @@
-from typing import Any, Iterator
-from typing_extensions import ParamSpec
-from typing import TypeVar, Callable
+"""Helper functions for the API wrapper"""
+
+# Standard library imports
 import datetime
+from functools import wraps
+from typing import (
+    Any,
+    Iterator,
+    TypeVar,
+    Callable
+)
+
+# Related third party imports
+from typing_extensions import ParamSpec
 
 T = TypeVar("T")
 P = ParamSpec("P")
-_cache = {}
+
+cache = {}
+ratelimit = {}
+
 
 def flatten(items: Any) -> Iterator:
     """
     A function to "completely" flatten a list. For example, itertools.chain() would flatten it once but with recursion
     this function flattens it completely til it's a list with no nested lists.
 
-    :param Any items: items to be flattened
-    :return: Iterator with flattened items (nothing nested)
+    Args:
+        items (Any): items to be flattened
+
+    Returns:
+        Iterator: Iterator with flattened items (nothing nested)
     """
     try:
-        if isinstance(items, str): raise TypeError
+        if isinstance(items, str):
+            raise TypeError
 
         for i in items:
             yield from flatten(i)
     except TypeError:
         yield items
 
-def cache_func(seconds: int = 300):
-    def real_decorator(func: Callable[[P], T]) -> Callable[[P], T]:
-        """
-        Cache functions. Automatically adds to dictionary. Cache time is 5 minutes by default.
 
-        :param func: function it's being decorated with
-        """
+def cache_func(seconds: int = 300) -> Callable:
+    """
+    Cache functions. Automatically adds to dictionary. Cache time is 5 minutes by default.
+
+    Args:
+        seconds (int): TTL for the cached version of the function
+
+    Returns:
+        Callable
+    """
+    def real_decorator(func: Callable[[P], T]) -> Callable[[P], T]:
+        """The actual decorator"""
+        @wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            if func.__name__ not in _cache:
-                _cache[func.__name__] = [None, None]
-            cache = _cache[func.__name__]
-            if cache[0] is None or datetime.datetime.now() > cache[1]:
+            """The wrapper"""
+            if func.__name__ not in cache:
+                cache[func.__name__] = [None, None]
+
+            cached_dict_value = cache[func.__name__]
+
+            if cached_dict_value[0] is None or datetime.datetime.now() > cached_dict_value[1]:
                 ret = func(*args, **kwargs)
-                _cache[func.__name__] = [ret, datetime.datetime.now() + datetime.timedelta(seconds=seconds)]
+                cache[func.__name__] = [ret, datetime.datetime.now() + datetime.timedelta(seconds=seconds)]
                 return ret
             else:
-                return cache[0]
+                return cached_dict_value[0]
+
         return wrapper
     return real_decorator
 
-def update_cached_function(corresponding_function: str):
-    def real_decorator(func: Callable[[P], T]) -> Callable[[P], T]:
-        """
-        Update a cached function and next time it's ran ensure it's not of cache and up-to-date.
 
-        :param func: function it's being decorated with
-        """
+def update_cached_function(corresponding_function: str) -> Callable:
+    """
+    Update a cached function and next time it's ran ensure it's not of cache and up-to-date.
+
+    Args:
+        corresponding_function (str): corresponding function to update
+
+    Returns:
+        Callable
+    """
+    def real_decorator(func: Callable[[P], T]) -> Callable[[P], T]:
+        """The actual decorator"""
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            """The wrapper"""
             ret = func(*args, **kwargs)
-            _cache[corresponding_function] = [None, None]
+            cache[corresponding_function] = [None, None]
             return ret
+
         return wrapper
     return real_decorator
