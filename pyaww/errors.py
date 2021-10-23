d03d671a1cf224421cdab4203bda9e70a1ee586e
Ammar <amarftw1@gmail.com>
2021-10-23 14:09:33 +0200

    [patch] handle circular imports, whole new way of handling errors too

--- pyaww/errors.py
+++ pyaww/errors.py
@@ -1,7 +1,43 @@
 """Errors for the module"""
 
+# Standard library imports
+
+from typing import Any, NoReturn
+
 
 class PythonAnywhereError(Exception):
     """A base exception, nothing special to it, used everywhere."""
 
-    pass
+
+class InvalidInfo(Exception):
+    """Exception for handling invalid tokens."""
+
+    def __init__(self, message: str, code: int) -> None:
+        super().__init__(message)
+        self.code = code
+
+
+class NotFound(InvalidInfo):
+    """Exception for handling 404's."""
+
+
+ERRORS_DICT = {
+    (401, "Invalid token."): InvalidInfo(
+        "Bad token provided, please check it at https://www.pythonanywhere.com/account/#api_token", 401
+    ),
+    (404, "Not found."): NotFound("Not found.", 404)
+}
+
+
+def raise_error(data: tuple[int, Any]) -> NoReturn:
+    """
+    Raise an appropriate error based on the response
+
+    Args:
+        data (tuple[Any]): data for the error
+    """
+
+    try:
+        raise ERRORS_DICT[data]
+    except KeyError:
+        raise PythonAnywhereError(data[1])
