866cebe0709d51fe36e09ea93d4a2869d841eebe
Ammar <amarftw1@gmail.com>
2021-10-24 23:52:19 +0200

    [update] add 1 more method

--- tests/submodules/test_always_on_task.py
+++ tests/submodules/test_always_on_task.py
@@ -9,3 +9,9 @@ def test_restart(always_on_task: AlwaysOnTask) -> None:
 
 def test_get_always_on_task_by_id(client: User, always_on_task: AlwaysOnTask) -> None:
     assert client.get_always_on_task_by_id(always_on_task.id) == always_on_task
+
+
+def test_update(always_on_task: AlwaysOnTask) -> None:
+    always_on_task.update(description="A")
+    always_on_task.update(description="B")
+    assert always_on_task.description == "B"
