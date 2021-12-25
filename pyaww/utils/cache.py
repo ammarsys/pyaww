574c3f0669b17fb8eb61d5cc862057ac549377c3
Ammar <amarftw1@gmail.com>
2021-12-25 13:56:21 +0100

    [chore] remove unnecessary functions as caching is handled elsewhere

--- pyaww/utils/cache.py
+++ pyaww/utils/cache.py
@@ -3,10 +3,8 @@
 # Standard library imports
 
 import datetime
-import inspect
 
-from functools import wraps
-from typing import TypeVar, Callable, Any, Union, Optional, Awaitable
+from typing import TypeVar, Any, Union, Optional
 from dataclasses import dataclass
 from collections.abc import Mapping
 
@@ -14,43 +12,40 @@ from collections.abc import Mapping
 
 from typing_extensions import ParamSpec
 
-
 T = TypeVar("T")
 P = ParamSpec("P")
 
 
 @dataclass(frozen=True, eq=True)
-class CachedRecord:
-    """Cache records; used for utils.cache.FunctionCache and utils.cache.cache_func"""
-
+class CachedResponse:
     params: tuple
     future: datetime.datetime
     ret: Any
 
 
-class FunctionCache(Mapping):
+class URLCache(Mapping):
     """
-    TTL cache for functions. This class must be used inside a regular dictionary in the following style,
+    TTL cache for URLs. This class should be used inside a regular dictionary in the following style,
 
-    >>> {"bar.foo": FunctionCache(...)}
+    >>> {'www.domain.com/api/v0/thing': URLCache(...)}
 
-    Keys/Values expire (get deleted) when you interact with them. In other words, when you call __getitem__,
-    __contains__ etc. the item will be checked to see if it's an instance of utils.cache.CachedRecord if it is,
+    Keys/Values expire (get deleted) when you interact with them. In other words, when you call __getitem__ or
+    __contains__, the item will be checked to see if it's an instance of utils.cache.CachedResponse if it is,
     see if the timedelta in the future is less than now, if it is, meaning its expired, pop the value and raise a
-    KeyError. Instances other than CachedRecord are ignored.
+    KeyError. Instances other than CachedResponse are ignored.
     """
 
     def __init__(
-        self, name: str, max_len: Union[float, int], to_cache: tuple = None
-    ):  # No need to default max_len since it gets passed from utils.cache.cache_func and it's defaulted there.
+            self, name: str, max_len: Union[float, int] = float('inf'), to_cache: tuple = None
+    ):  # Defaulting max_len because of the update_cache function
         self.name = name
         self.max_len = max_len
-        self.cache: dict[tuple, CachedRecord] = {}
+        self.cache: dict[tuple, CachedResponse] = {}
 
         if to_cache:
             self.__setitem__(*to_cache)
 
-    def __check_if_expired(self, item: CachedRecord) -> Optional[None]:
+    def __check_if_expired(self, item: CachedResponse) -> Optional[None]:
         """Method to see if a key has expired, private because it can mess with caching."""
         if item.future <= datetime.datetime.now():
             self.cache.pop(item.params)
@@ -59,7 +54,7 @@ class FunctionCache(Mapping):
     def __getitem__(self, item):
         item = self.cache[item]
 
-        if isinstance(item, CachedRecord):
+        if isinstance(item, CachedResponse):
             self.__check_if_expired(item)
 
         return item
@@ -67,7 +62,7 @@ class FunctionCache(Mapping):
     def __contains__(self, item):
         try:
             item = self.cache[item]
-            if isinstance(item, CachedRecord):
+            if isinstance(item, CachedResponse):
                 if not self.__check_if_expired(item):
                     return item in self.cache
         except KeyError:
@@ -85,85 +80,3 @@ class FunctionCache(Mapping):
 
     def __iter__(self):
         yield from self.cache
-
-
-def cache_func(
-    seconds: Union[int, float] = 300, max_len: Union[int, float] = float("inf")
-) -> Callable:
-    """
-    Cache functions. Cache time is 5 minutes by default.
-
-    The user is able to disable this caching via pyaww.User.use_cache or disable a specific method from being cached
-    by appending the qualname of the function to the pyaww.User.disable_cache. Cached instances are stored in a
-    dictionary which keys are the functions qualname(s) and it's values an utils.cache.FunctionCache object.
-    The time-to-live of values is handled inside the utils.cache.FunctionCache objects.
-
-    Passed function in this deco must be a class instance (have self as it's first attribute) and that class must
-    contain "disable_cache", "use_cache", "cache" and "lock" instance variables. Please see how they're defined in the
-    example bellow.
-
-    Examples:
-        >>> import asyncio
-        >>>
-        >>> class Foo:
-        >>>    def __init__(self):
-        >>>         self.cache = {}
-        >>>         self.use_cache = True
-        >>>         self.disable_cache = ()
-        >>>         self.lock = asyncio.Lock()
-        >>>
-        >>>    @cache_func(seconds=1)
-        >>>    async def bar(self, baz):
-        >>>         print("I was ran.")
-        >>>         return baz + 1
-
-    Args:
-        max_len (Union[float, int] = float('inf')): maximum number of cached records
-        seconds (Union[int, float]): TTL for the cached version of the function
-
-    Returns:
-        Callable
-    """
-
-    def real_decorator(func: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
-        """The actual decorator, the function is expected to be an awaitable."""
-
-        @wraps(func)
-        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> Awaitable[T]:
-            """The wrapper"""
-
-            if not args[0].use_cache or func.__qualname__ in args[0].disable_cache:
-                return await func(*args, **kwargs)
-
-            cache = args[0].cache
-            lock = args[0].lock
-
-            siggy = inspect.signature(func).bind(*args, **kwargs)
-            siggy.apply_defaults()
-            params = tuple(siggy.arguments.values())
-
-            time_ = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
-
-            if func.__qualname__ in cache:
-                try:
-                    return cache[func.__qualname__][params].ret
-                except KeyError:
-                    ret = await func(*args, **kwargs)
-                    async with lock:
-                        cache[func.__qualname__][params] = CachedRecord(
-                            params, time_, ret
-                        )
-                    return ret
-            else:
-                ret = await func(*args, **kwargs)
-                async with lock:
-                    cache[func.__qualname__] = FunctionCache(
-                        name=func.__qualname__,
-                        to_cache=(params, CachedRecord(params, time_, ret)),
-                        max_len=max_len,
-                    )
-                return ret
-
-        return wrapper
-
-    return real_decorator
