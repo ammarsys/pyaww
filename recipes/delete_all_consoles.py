63d08a66414fac65a5245e00ece584915113d50a
Ammar <amarftw1@gmail.com>
2022-01-13 17:22:28 +0100

    [refactor] functions were split, update example

--- recipes/delete_all_consoles.py
+++ recipes/delete_all_consoles.py
@@ -5,6 +5,6 @@ client = pyaww.User(..., ...)
 
 async def kill_all_personal_consoles() -> None:
     consoles = await client.consoles()
-    for console in consoles["personal"]:
+    for console in consoles:
         print(f"deleting console with the name {console.name}...")
         await console.delete()
