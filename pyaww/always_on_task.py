558582c12ce1051a55c2c9b62af53e17ae566ebc
Ammar <amarftw1@gmail.com>
2022-01-21 01:52:39 +0100

    [refactor] implement the methods without caching

--- pyaww/always_on_task.py
+++ pyaww/always_on_task.py
@@ -10,19 +10,44 @@ if TYPE_CHECKING:
     from .user import User
 
 
-class AlwaysOnTask(SchedTask):
+class AlwaysOnTask:
     """
     Implements AlwaysOnTask endpoints.
 
     See Also https://help.pythonanywhere.com/pages/AlwaysOnTasks/
     """
 
+    url: str
+    id: int
+    ...
+
     def __init__(self, resp: dict, user: "User") -> None:
-        super().__init__(resp, user)
+        vars(self).update(resp)
+        self._user = user
 
     async def restart(self) -> None:
         """Restart an always_on task."""
         await self._user.request("POST", self.url + "restart/")
+
+    async def delete(self) -> None:
+        """Delete the task."""
+        await self._user.request("DELETE", self.url)
+
+    async def update(self, **kwargs) -> None:
+        """
+        Updates the task. All times are in UTC.
+
+        Args:
+            **kwargs: command str, minute str, hour str, interval str, description str,
+            enabled bool
+
+        Examples:
+            >>> user = User(...)
+            >>> task = await user.get_always_on_task_by_id(...)
+            >>> await task.update(command='cd')
+        """
+        await self._user.request("PATCH", self.url, data=kwargs)
+        vars(self).update(kwargs)
 
     def __str__(self):
         return self.url
